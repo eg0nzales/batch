839f844c0cc5ee83b0dfdbfe723950f9d7c2258b
eg0nzales <egonzales@nrchealth.com>
2025-04-16 16:35:03 -0400

    Update TestFiles.bat

--- TestFiles.bat
+++ TestFiles.bat
@@ -1,5 +1,4 @@
 @echo off
 del POST_TEST_syndicate.py
-curl -L -O https://raw.githubusercontent.com/eg0nzales/python/main/POST_TEST_syndicate.py
+curl -L -O https://raw.githubusercontent.com/eg0nzales/python/main/TEST.py
 python POST_TEST_syndicate.py
-pause
