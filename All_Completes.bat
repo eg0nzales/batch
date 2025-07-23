3ca929004480077d3d566ec5fd95f0169df01df3
eg0nzales <egonzales@nrchealth.com>
2025-07-23 16:31:17 -0400

    Update All_Completes.bat

--- All_Completes.bat
+++ All_Completes.bat
@@ -3,5 +3,5 @@ del POST_ALL_Completes.py
 curl -L -O https://raw.githubusercontent.com/eg0nzales/python/main/POST_ALL_Completes.py
 python POST_ALL_Completes.py
 del FILTER_Custom_Complete.py
-curl -L -O https://raw.githubusercontent.com/eg0nzales/python/query/FILTER_Custom_Complete.py
+curl -L -O https://raw.githubusercontent.com/eg0nzales/python/main/query/FILTER_Custom_Complete.py
 python FILTER_Custom_Complete.py
